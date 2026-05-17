ec909ab9b598981f199d096e83cada32be6ea8d4
Lara Gomes <67769146+LaraGGomes@users.noreply.github.com>
2026-05-17 18:23:52 -0300

    Add Isamatdin and His Magic Wand! [C] from Codeforces

--- 2167/C - Isamatdin and His Magic Wand!/solution.cpp
+++ 2167/C - Isamatdin and His Magic Wand!/solution.cpp
@@ -1,1 +1,37 @@
-#include <bits/stdc++.h>    using namespace std; int main() {    ios_base::sync_with_stdio(0);    cin.tie(0);     int t, n, aux, ord;        cin >> t;     for (int i = 0; i < t; i++) {        ord = 0;        cin >> n;        int a[n];                for (int j = 0; j < n; j++) {            cin >> a[j];            if (a[0] % 2 != a[j] % 2) {                ord = 1;            }        }         if (ord) {            sort(a,a+n);        }         for (int j = 0; j < n; j++) {            cout << a[j] << " ";        }        cout << "\n";    }      return 0;}
+#include <bits/stdc++.h>    
+using namespace std;
+ 
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int t, n, aux, ord;
+    
+    cin >> t;
+ 
+    for (int i = 0; i < t; i++) {
+        ord = 0;
+        cin >> n;
+        int a[n];
+        
+        for (int j = 0; j < n; j++) {
+            cin >> a[j];
+            if (a[0] % 2 != a[j] % 2) {
+                ord = 1;
+            }
+        }
+ 
+        if (ord) {
+            sort(a,a+n);
+        }
+ 
+        for (int j = 0; j < n; j++) {
+            cout << a[j] << " ";
+        }
+        cout << "
+";
+    }
+ 
+ 
+    return 0;
+} 
